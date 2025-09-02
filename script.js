d95497c29327d3ffa417243faaf892286f0ad02f
kelimunik <keli.vianna@escola.pr.gov.br>
2025-09-02 16:51:50 -0300

    aula do dia 02-09

---
+++ script.js
@@ -0,0 +1,11 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave')
+botaoMostraPalavras.addEventListener('click', mostraPalavraschave);
+
+function mostraPalavraschave(){
+    const texto = document.querySelector('#entrada-de-texto').value:
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavras = texto.split(" ");
+
+campoResultado.textContent = palavras.join(",");
+}
+
